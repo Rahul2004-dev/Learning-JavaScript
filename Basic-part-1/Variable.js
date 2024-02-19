17537ef1c74a37b2c54ec2582a89e8c14dd575b9
Rahul Gupta <148553584+Rahul2004-dev@users.noreply.github.com>
2024-02-19 07:59:52 +0000

    Code running Setup environment

---
+++ Basic-part-1/Variable.js
@@ -0,0 +1,8 @@
+// Basically three type of Variable are exist in JavaScript (let, var, const{Constant Variabe}), detailes are below
+
+/*this type of variables value are any time will be changed */
+let Rahul = "Rahul"  
+var valuesInNumber = 56
+
+console.log(Rahul);
+console.log(valuesInNumber);
